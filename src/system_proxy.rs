c96ccde54c22c45cf9ed4b25d444127e4af51c67
Hassan Q <hassanak813@outlook.com>
2026-05-26 15:45:45 +0330

    add linux

--- src/system_proxy.rs
+++ src/system_proxy.rs
@@ -7,6 +7,7 @@ use crate::config::Config;
 const LINUX_SCRIPT: &str = include_str!("../scripts/proxy_set_linux_sh");
 const MACOS_SCRIPT: &str = include_str!("../scripts/proxy_set_osx_sh");
 
+#[cfg(all(not(target_os = "android"), not(target_os = "ios")))]
 pub fn enable_system_proxy(cfg: &Config) -> Result<(), Error> {
     #[cfg(target_os = "windows")]
     {
@@ -26,6 +27,7 @@ pub fn enable_system_proxy(cfg: &Config) -> Result<(), Error> {
             Err(e) => Err(e),
         }
     }
+
     #[cfg(target_os = "macos")]
     {
         run_script(
@@ -39,8 +41,23 @@ pub fn enable_system_proxy(cfg: &Config) -> Result<(), Error> {
             )
         })
     }
+
+    #[cfg(all(unix, not(target_os = "macos")))]
+    {
+        run_script(
+            LINUX_SCRIPT,
+            &["manual", &cfg.listen_host, &cfg.lister_port.to_string()],
+        )
+        .map_err(|e| {
+            Error::new(
+                ErrorKind::Other,
+                format!("Failed to enable system proxy: {}", e),
+            )
+        })
+    }
 }
 
+#[cfg(all(not(target_os = "android"), not(target_os = "ios")))]
 pub fn disable_system_proxy() -> Result<(), Error> {
     #[cfg(target_os = "windows")]
     {
@@ -57,6 +74,7 @@ pub fn disable_system_proxy() -> Result<(), Error> {
             Err(e) => Err(e),
         }
     }
+
     #[cfg(target_os = "macos")]
     {
         run_script(MACOS_SCRIPT, &["clear"]).map_err(|e| {
@@ -66,8 +84,19 @@ pub fn disable_system_proxy() -> Result<(), Error> {
             )
         })
     }
+
+    #[cfg(all(unix, not(target_os = "macos")))]
+    {
+        run_script(LINUX_SCRIPT, &["none"]).map_err(|e| {
+            Error::new(
+                ErrorKind::Other,
+                format!("Failed to enable system proxy: {}", e),
+            )
+        })
+    }
 }
 
+#[cfg(all(not(target_os = "android"), not(target_os = "ios")))]
 fn run_script(script_content: &str, args: &[&str]) -> std::io::Result<()> {
     let script_path = std::env::temp_dir().join("proxy_script.sh");
     fs::write(&script_path, script_content)?;
